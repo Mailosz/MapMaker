6aba274fbb2fe259d7b8688685070ad6ea7256da
Miłosz Osadczuk <m2ilosz@outlook.com>
2026-03-14 00:15:05 +0100

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+// import {Map, View, OSM, TileLayer} from './ol/dist/ol.js';
+
+// new Map({
+//   layers: [
+//     new TileLayer({source: new OSM()}),
+//   ],
+//   view: new View({
+//     center: [0, 0],
+//     zoom: 2,
+//   }),
+//   target: 'map-container',
+// });
